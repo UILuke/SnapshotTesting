f21251199668c879d333e34abbcf995b5b5dfd0c
UILuke <luke.liu.au@gmail.com>
2024-11-23 21:38:09 +1100

    init project

---
+++ SnapshotTesting/SnapshotTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotTestingApp.swift
+//  SnapshotTesting
+//
+//  Created by Luke Liu on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
